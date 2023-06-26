4e0455b339dbc0cea905e2ecae6995bd3b012767
tanvirgeek <tanvirgeek@gmail.com>
2023-06-26 07:07:37 +0600

    CRUD done

--- app/api/blog/[id]/route.ts
+++ app/api/blog/[id]/route.ts
@@ -13,12 +13,42 @@ export const GET = async (req: Request, res: NextResponse) => {
       return NextResponse.json({ message: "Not Found" }, { status: 404 });
     return NextResponse.json({ message: "Success", post }, { status: 200 });
   } catch (error) {
-    return NextResponse.json({ message: "Success", error }, { status: 500 });
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
   } finally {
     await prisma.$disconnect();
   }
 };
 
-export const PUT = async (req: Request, res: NextResponse) => {};
+export const PUT = async (req: Request, res: NextResponse) => {
+  try {
+    const id = req.url.split("/blog/")[1];
+    const { title, description } = await req.json();
+    await main();
+    const post = await prisma.post.update({
+      data: { title, description },
+      where: { id },
+    });
+    if (!post) return NextResponse.json({ message: "Error" }, { status: 500 });
+    return NextResponse.json({ message: "Success", post }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
 
-export const DELETE = async (req: Request, res: NextResponse) => {};
+export const DELETE = async (req: Request, res: NextResponse) => {
+  try {
+    const id = req.url.split("/blog/")[1];
+    await main();
+    const post = await prisma.post.delete({
+      where: { id },
+    });
+    if (!post) return NextResponse.json({ message: "Error" }, { status: 500 });
+    return NextResponse.json({ message: "Success", post }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
